f32c8d2067572851f259e92929b4eb05438ab1b0
Kirill I. A <ghztrikz1337@gmail.com>
2023-04-02 06:25:35 +0300

    few changes

--- app/src/main/java/com/smechotech/onboarding/ui/features/UserPreferences.kt
+++ app/src/main/java/com/smechotech/onboarding/ui/features/UserPreferences.kt
@@ -5,14 +5,14 @@ import com.smechotech.onboarding.MainActivity
 const val FIRST_TIME_LAUNCH = "FIRST_TIME_LAUNCH"
 const val USER_AUTHORIZED = "USER_AUTHORIZED"
 
-fun firstTimeLaunch(): Boolean {
-    val firsTimeLaunch = MainActivity.sharedPreferences.getBoolean(FIRST_TIME_LAUNCH, true)
-    with(MainActivity.sharedPreferences.edit()) {
+fun firstTimeLaunch() = MainActivity.sharedPreferences.getBoolean(FIRST_TIME_LAUNCH, true)
+
+fun showOnBoarding() = firstTimeLaunch()
+fun userPassOnBoarding() = with(MainActivity.sharedPreferences.edit()) {
         putBoolean(FIRST_TIME_LAUNCH, false)
         apply()
     }
-    return firsTimeLaunch
-}
+
 
 fun isUserNotAuthorized() = MainActivity.sharedPreferences.getBoolean(USER_AUTHORIZED, false)
 fun userPassAuthorization() = with(MainActivity.sharedPreferences.edit()) {
